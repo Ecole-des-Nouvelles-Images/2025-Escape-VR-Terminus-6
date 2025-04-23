9c0105ad09dafb0f73572f659f7fa8ef110a3251
ElkTargets <117286167+ElkTargets@users.noreply.github.com>
2025-04-23 16:48:37 +0200

    Working on CD animation, theorically working, see in build

--- Assets/RAW IMPORT (Avant Tri)/DEV/Vincent/Scripts/TV/CdPlayer.cs
+++ Assets/RAW IMPORT (Avant Tri)/DEV/Vincent/Scripts/TV/CdPlayer.cs
@@ -23,17 +23,24 @@ public class CdPlayer : MonoBehaviour
         if (other.GetComponent<XRGrabInteractable>().isSelected) { CdGhost.enabled = true; }
         else {
             CdGhost.enabled = false;
+            StartCoroutine(CDIn(other.gameObject));
             other.GetComponent<Rigidbody>().isKinematic = true;
             other.GetComponent<Rigidbody>().useGravity = false;
             other.GetComponent<MeshRenderer>().enabled = false;
-            _videoPlayer.Play();
-            _cd = other.gameObject;
-            _cd.transform.position = Anchor.position;
-            _cdIn = true;
+            
         }
     }
     private void Update() {
         if (_videoPlayer.time >= _videoPlayer.clip.length && _cdIn) { _cdIn = false; EjectCd(); }
     }
     private void EjectCd() { _cd.GetComponent<Rigidbody>().AddForce(new Vector3(0,0,15), ForceMode.Impulse); }
+
+    private IEnumerator CDIn(GameObject cd) {
+        _cd.transform.position = Anchor.position;
+        _cd = cd;
+        _cdIn = true;
+        GetComponent<Animator>().SetTrigger("TriggerCdIn");
+        yield return new WaitForSeconds(0.33f);
+        _videoPlayer.Play();
+    }
 }
