d5919b51ccc368b7f8af4dc0cd6eaf4e7d1a572f
Bastien Santé | Bas7ien <118430107+BastienSANTE@users.noreply.github.com>
2025-05-14 17:30:26 +0200

    Heavily optimized rendering, added distant btn

--- Assets/RAW IMPORT (Avant Tri)/DEV/Bastien/SceneSwitcher.cs
+++ Assets/RAW IMPORT (Avant Tri)/DEV/Bastien/SceneSwitcher.cs
@@ -1,22 +1,8 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void StartGame(){
 	SceneManager.LoadScene("GameScene_Alt_Bastien", LoadSceneMode.Single);
     }
