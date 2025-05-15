e2c1c7640d476f74a62545342f94baedcc549a1b
Bastien Santé | Bas7ien <118430107+BastienSANTE@users.noreply.github.com>
2025-05-15 16:44:13 +0200

    Added Raycasts to Main Menu, Transition

--- Assets/RAW IMPORT (Avant Tri)/DEV/Bastien/SceneSwitcher.cs
+++ Assets/RAW IMPORT (Avant Tri)/DEV/Bastien/SceneSwitcher.cs
@@ -4,7 +4,7 @@ using UnityEngine.SceneManagement;
 public class SceneSwitcher : MonoBehaviour
 {
     public void StartGame(){
-	SceneManager.LoadScene("GameScene_Alt_Vincent", LoadSceneMode.Single);
+	SceneManager.LoadScene("GameScene_Alt_Bastien", LoadSceneMode.Single);
     }
 
     public void StartTransitionAnimation()
