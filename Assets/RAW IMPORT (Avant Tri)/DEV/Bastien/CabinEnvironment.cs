49726cf72e0896dce657186c5721af5e230b2ad1
Bastien Santé | Bas7ien <118430107+BastienSANTE@users.noreply.github.com>
2025-05-16 12:00:20 +0200

    Started modifying cabin switch (Incomplete!)

--- Assets/RAW IMPORT (Avant Tri)/DEV/Bastien/CabinEnvironment.cs
+++ Assets/RAW IMPORT (Avant Tri)/DEV/Bastien/CabinEnvironment.cs
@@ -1,17 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
-public class CabinEnvironment : MonoBehaviour
-{
+public class CabinEnvironment : MonoBehaviour {
     [SerializeField] private GameObject _back;
     [SerializeField] private GameObject _portal;
-
     [SerializeField] private Enigma _enigma;
-    
+
+    [SerializeField] private Portal _switchPortal;
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         _portal.SetActive(false);
         SetupEventListeners();
     }
@@ -22,15 +19,21 @@ public class CabinEnvironment : MonoBehaviour
         
     }
 
-    private void SetupEventListeners()
-    {
+    private void SetupEventListeners() {
         _enigma.Solve.AddListener(OnEnigmaSolved);
+        //PortalCrossed.AddListener(ChangeCabin);
+        _switchPortal.CabinSwitch.AddListener(ChangeCabin);
     }
 
-    private void OnEnigmaSolved()
-    {
+    private void OnEnigmaSolved() {
         _back.SetActive(false);
         _portal.SetActive(true);
+    }
+
+    private void ChangeCabin() {
+        for (int i = 0; i < this.transform.childCount; i++) {
+            transform.GetChild(i).gameObject.SetActive(false);
+        }
     }
     
 }
