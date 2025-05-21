3bdb2506c7260706cfe5e9ea705c5022db20d533
Elk Targets <v.meneroud@nouvellesimages.xyz>
2025-05-21 16:47:28 +0200

    Eject CD

--- Assets/RAW IMPORT (Avant Tri)/DEV/Vincent/Scripts/TV/CdPlayer.cs
+++ Assets/RAW IMPORT (Avant Tri)/DEV/Vincent/Scripts/TV/CdPlayer.cs
@@ -12,19 +12,24 @@ public class CdPlayer : MonoBehaviour {
     private bool _isCdIn;
     private float _playbackTime;
 
+    private BoxCollider _boxCollider;
     private VideoPlayer _videoPlayer;
 
     private void Awake() {
         _videoPlayer = GetComponent<VideoPlayer>();
         _cdGhost.enabled = false;
+        _boxCollider = GetComponent<BoxCollider>();
     }
 
     private void Update() {
         _playbackTime = (float)_videoPlayer.time;
-        if (_playbackTime >= _videoPlayer.clip.length - .5f) {
+        if (_playbackTime >= _videoPlayer.clip.length - .5f && _isCdIn) {
             _isCdIn = false;
             _enigma.Solve.Invoke();
-            EjectCd();
+            if (_currentCd != null)
+            {
+                EjectCd();
+            }
         }
     }
 
@@ -41,14 +46,37 @@ public class CdPlayer : MonoBehaviour {
     private void EjectCd() {
         if (_currentCd != null) {
             var rb = _currentCd.GetComponent<Rigidbody>();
-            rb.AddForce(Vector3.forward * 15, ForceMode.Impulse);
+            _boxCollider.enabled = false;
+            _currentCd.GetComponent<Collider>().enabled = false;
+            rb.isKinematic = false;
+            rb.constraints = RigidbodyConstraints.None;
+            rb.constraints = RigidbodyConstraints.FreezePositionX;
+            rb.constraints = RigidbodyConstraints.FreezePositionY;
+            rb.useGravity = true;
+            rb.AddForce(new Vector3(0,0,-1f) * 0.5f, ForceMode.Impulse);
+            StartCoroutine(EjectCdMiniTimer());
         }
+    }
+
+    private IEnumerator EjectCdMiniTimer()
+    {
+        var rb = _currentCd.GetComponent<Rigidbody>();
+        rb.isKinematic = false;
+        yield return new WaitForSeconds(1f);
+        _boxCollider.enabled = true;
+        rb.velocity = Vector3.zero;
+        rb.constraints = RigidbodyConstraints.None;
+        GetComponent<Animator>().SetTrigger("Reset");
+        _currentCd.GetComponent<Collider>().enabled = true;
+        _currentCd.transform.parent = null;
+        _currentCd = null;
+        _isCdIn = false;
+        
     }
 
     private IEnumerator InsertCd(GameObject cd) {
         var rb = cd.GetComponent<Rigidbody>();
         var collider = cd.GetComponent<Collider>();
-
         collider.enabled = false;
         rb.useGravity = false;
         rb.isKinematic = true;
